e7a157dbc9ff4d6128fe41409a21294dc9eadea3
saikrishnacom <palaparthisai.77@gmail.com>
2023-08-03 15:02:08 +0530

    Added logging

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
     )
     scr_dir = os.environ['SRC_DIR']
     # tgt_dir = os.environ['TGT_DIR']
-    src_file_pattern = os.environ.setdefault('SET_FILE_PATTERN','NYSE_*.txt.gz')
+    src_file_pattern = os.environ.setdefault('SET_FILE_PATTERN','NYSE*.txt.gz')
     src_file_name = glob.glob(f'{scr_dir}/{src_file_pattern}')
     tgt_file_names=[file.replace('txt','json').replace('nyse_data','nyse_json') for file in src_file_name]
     logging.info('file format conversion is started')
